e7312f544fc17c5fed7e1f2c17e2f26d4c61976f
Administrator <Administrator@Eng-ShoRouK-PC>
2016-03-07 05:17:14 +0200

    Dao_auth_category

---
+++ OnlineBookShop/src/java/com/daos/Author_Dao.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daos;
+
+import com.beans.Author;
+import com.utilts.DbConnctor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Author_Dao {
+
+    private static final String SQL_READ = "SELECT * FROM AUTHOR";
+    private static final String SQL_INSERT = "INSERT INTO AUTHOR(AUTH_ID,AUTH_NAME,AUTH_ABOUT,AUTH_IMG)"
+            + "VALUES(AUTHOR_SEQ_TMP.NEXTVAL,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE AUTHOR SET AUTH_NAME=? WHERE AUTH_ID=?";
+    private static final String SQL_DELETE = "DELETE FROM BOOK WHERE AUTH_ID=?";
+
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+
+    public Author_Dao() {
+
+    }
+
+    public boolean add(Author authorObj) throws SQLException {
+        try {
+
+            DbConnctor.openConnection();
+            statement = connection.prepareStatement(SQL_INSERT);
+            statement.setInt(1, authorObj.getAuthId());
+            statement.setString(2, authorObj.getAuthName());
+            statement.setString(3, authorObj.getAuthAbout());
+            statement.setString(4, authorObj.getAuthImg());
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return false;
+    }
+
+    public boolean update(Author authorObj) throws SQLException {
+
+        try {
+            DbConnctor.openConnection();
+            statement = connection.prepareStatement(SQL_UPDATE);
+            statement.setString(1, authorObj.getAuthName());
+            statement.setInt(2, authorObj.getAuthId());
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return false;
+    }
+
+    public boolean delete(int authorID) throws SQLException {
+
+        try {
+            DbConnctor.openConnection();
+            statement = connection.prepareStatement(SQL_DELETE);
+            statement.setInt(1, authorID);
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return false;
+    }
+
+    public List<Author> readAll() throws SQLException {
+        ArrayList<Author> authorList = new ArrayList();
+        try {
+            DbConnctor.openConnection();
+            Author author = null;
+            statement = connection.prepareStatement(SQL_READ);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                author = new Author();
+                author.setAuthId(resultSet.getInt(1));
+                author.setAuthName(resultSet.getString(2));
+                author.setAuthAbout(resultSet.getString(3));
+                author.setAuthImg(resultSet.getString(4));
+                authorList.add(author);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return authorList;
+    }
+
+}
---
+++ OnlineBookShop/src/java/com/daos/Category_Dao.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daos;
+
+import com.beans.Category;
+import com.utilts.DbConnctor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Category_Dao {
+    private static final String SQL_READ = "SELECT * FROM CATEGORY";
+    private static final String SQL_INSERT = "INSERT INTO CATEGORY(CAT_ID,CAT_NAME)"
+            + "VALUES(CATEGORY_SEQ_TMP.NEXTVAL,?)";
+    private static final String SQL_UPDATE = "UPDATE CATEGORY SET CAT_NAME=? WHERE CAT_ID=?";
+    private static final String SQL_DELETE = "DELETE FROM CATEGORY WHERE CAT_ID=?";
+
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+
+    public Category_Dao() {
+
+    }
+
+    public boolean add(Category categoryObj) throws SQLException {
+        try {
+
+            DbConnctor.openConnection();
+            statement = connection.prepareStatement(SQL_INSERT);
+            statement.setInt(1, categoryObj .getCatId());
+            statement.setString(2, categoryObj.getCatName());
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return false;
+    }
+
+    public boolean update(Category categoryObj) throws SQLException {
+
+        try {
+            DbConnctor.openConnection();
+            statement = connection.prepareStatement(SQL_UPDATE);
+            statement.setString(1, categoryObj.getCatName());
+            statement.setInt(2, categoryObj.getCatId());
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return false;
+    }
+
+    public boolean delete(int authorID) throws SQLException {
+
+        try {
+            DbConnctor.openConnection();
+            statement = connection.prepareStatement(SQL_DELETE);
+            statement.setInt(1, authorID);
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return false;
+    }
+
+    public List<Category> readAll() throws SQLException {
+        ArrayList<Category> catgList = new ArrayList();
+        try {
+            DbConnctor.openConnection();
+            Category category = null;
+            statement = connection.prepareStatement(SQL_READ);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                category = new Category();
+                category.setCatId(resultSet.getInt(1));
+                category.setCatName(resultSet.getString(2));
+                catgList.add(category);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DbConnctor.closeConnection();
+        }
+        return catgList;
+    }
+
+
+}
