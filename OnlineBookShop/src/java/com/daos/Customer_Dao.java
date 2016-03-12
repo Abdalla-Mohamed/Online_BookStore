9fa072d38134502a6e2a2b4c897b30b63206da9c
habdalnaser <habdalnaser@gmail.com>
2016-03-12 02:50:11 +0200

    exchange in customer dao , completing Login,Signup,Logout servlets modifing inde,checkout jsps creating profile,update profile jsps

--- OnlineBookShop/src/java/com/daos/Customer_Dao.java
+++ OnlineBookShop/src/java/com/daos/Customer_Dao.java
@@ -14,48 +14,49 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ElGazzar
  */
 public class Customer_Dao {
+
     Connection connection = null;
     PreparedStatement pstatement = null;
     Statement statement = null;
     ResultSet resultSet = null;
-    
-    public Customer_Dao()
-    {    
+
+    public Customer_Dao() {
     }
-    
-    public boolean validEmail(String email) throws SQLException
-    {
+
+    public boolean validEmail(String email) throws SQLException {
         try {
             connection = DbConnctor.openConnection();
             statement = connection.createStatement();
-            String validSQL = "SELECT * FROM CUSTOMER WHERE C_EMAIL = '"+email+"'";
+            String validSQL = "SELECT * FROM CUSTOMER WHERE C_EMAIL = '" + email + "'";
             resultSet = statement.executeQuery(validSQL);
-            
+
             if (resultSet.next()) {
                 return true;
             } else {
                 return false;
             }
-           
+
         } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally{
+        } finally {
             DbConnctor.closeConnection();
         }
         return true;
     }
-    
-    public boolean login(String email, String passwd) throws SQLException
-    {
-         try {
-            
+
+    public Customer login(String email, String passwd) throws SQLException {
+
+        Customer customer = null;
+
+        try {
             connection = DbConnctor.openConnection();
             statement = connection.createStatement();
             String query = "SELECT * FROM CUSTOMER WHERE C_EMAIL ='" + email + "' AND C_PASSWORD ='" + passwd + "' ";
@@ -64,9 +65,10 @@ public class Customer_Dao {
 
             if (resultSet.next()) {
 
-                return true;
+                customer = getCustomer(resultSet);
+
             } else {
-                return false;
+                return null;
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -74,19 +76,24 @@ public class Customer_Dao {
         } finally {
             DbConnctor.closeConnection();
         }
-         return true;
+        return customer;
     }
-    
-    public boolean signUp(Customer c) throws SQLException
-    {
-        if(!validEmail(c.getCEmail()))
-        {
-            try {                
+
+    public boolean signUp(Customer c) throws SQLException {
+        
+        Boolean valid=false;
+        
+        if (!validEmail(c.getCEmail())) {
+            try {
+                valid=true;
+                
+                c.setCCredit(0);
+
                 connection = DbConnctor.openConnection();
-                String sql= "INSERT INTO CUSTOMER(C_NAME,C_EMAIL,C_PASSWORD,C_JOB,C_ADDRESS,C_MOBILE,C_CREDIT,C_ID)"
+                String sql = "INSERT INTO CUSTOMER(C_NAME,C_EMAIL,C_PASSWORD,C_JOB,C_ADDRESS,C_MOBILE,C_CREDIT,C_ID)"
                         + "VALUES(?,?,?,?,?,?,?,CUSTOMER_SEQ_TMP.NEXTVAL)";
                 pstatement = connection.prepareStatement(sql);
-                
+
                 pstatement.setString(1, c.getCName());
                 pstatement.setString(2, c.getCEmail());
                 pstatement.setString(3, c.getCPassword());
@@ -94,23 +101,26 @@ public class Customer_Dao {
                 pstatement.setString(5, c.getCAddress());
                 pstatement.setString(6, c.getCMobile());
                 pstatement.setInt(7, c.getCCredit());
-                
+
                 pstatement.executeUpdate();
-                
+
             } catch (SQLException ex) {
                 ex.printStackTrace();
-            }
-            finally {
+            } finally {
 
                 DbConnctor.closeConnection();
             }
             
         }
-        return true;
-    }
-    
-    public List<Customer> getAllCustomers() throws SQLException {
         
+       
+        return valid;
+        
+         
+    }
+
+    public List<Customer> getAllCustomers() throws SQLException {
+
         List<Customer> customerList = new ArrayList<Customer>();
         try {
             connection = DbConnctor.openConnection();
@@ -118,7 +128,7 @@ public class Customer_Dao {
             resultSet = statement.executeQuery("SELECT * FROM CUSTOMER");
             while (resultSet.next()) {
                 Customer customer = new Customer();
-                
+
                 customer.setCName(resultSet.getString("C_NAME"));
                 customer.setCEmail(resultSet.getString("c_email"));
                 customer.setCPassword(resultSet.getString("c_password"));
@@ -131,24 +141,22 @@ public class Customer_Dao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             DbConnctor.closeConnection();
         }
 
         return customerList;
     }
-    
-    public ArrayList findByName(String name) throws SQLException 
-  {
-      
-      ArrayList arr = new ArrayList();
-      try {
+
+    public ArrayList findByName(String name) throws SQLException {
+
+        ArrayList arr = new ArrayList();
+        try {
             connection = DbConnctor.openConnection();;
             pstatement = connection.prepareStatement("SELECT * FROM CUSTOMER WHERE C_NAME =?");
             pstatement.setString(1, name);
             resultSet = pstatement.executeQuery();
-            while(resultSet.next())
-            {
+            while (resultSet.next()) {
                 int id = resultSet.getInt("C_ID");
                 String cName = resultSet.getString("C_NAME");
                 String cEmail = resultSet.getString("c_email");
@@ -157,42 +165,59 @@ public class Customer_Dao {
                 String cAddress = resultSet.getString("c_address");
                 String cMobile = resultSet.getString("c_mobile");
                 int cCredit = resultSet.getInt("c_credit");
-                
+
                 Customer c = new Customer(id, cName, cEmail, passwd, cJob, cAddress, cMobile, cCredit, null, null, null);
                 arr.add(c);
             }
-                
+
         } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-      finally {
+        } finally {
 
-                DbConnctor.closeConnection();
-            }
-      return arr;
-  }
-    
+            DbConnctor.closeConnection();
+        }
+        return arr;
+    }
+
     public boolean deleteCustomer(int CustId) throws SQLException {
-        try
-        {
+        try {
             connection = DbConnctor.openConnection();
             statement = connection.createStatement();
-            String query = "DELETE FROM CUSTOMER WHERE C_ID='"+CustId+"'";
-              
-            statement.executeUpdate(query);
-                
-        }
-        catch(SQLException ex)
-        {
-            ex.printStackTrace();
-            
-        }
-        finally {
+            String query = "DELETE FROM CUSTOMER WHERE C_ID='" + CustId + "'";
 
-                DbConnctor.closeConnection();
-            }
+            statement.executeUpdate(query);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        } finally {
+
+            DbConnctor.closeConnection();
+        }
         return true;
     }
-    
-    
+
+    private Customer getCustomer(ResultSet result) {
+
+        Customer customer = null ;
+        try {
+
+            
+                int id = resultSet.getInt("C_ID");
+                String cName = resultSet.getString("C_NAME");
+                String cEmail = resultSet.getString("c_email");
+                String passwd = resultSet.getString("c_password");
+                String cJob = resultSet.getString("c_job");
+                String cAddress = resultSet.getString("c_address");
+                String cMobile = resultSet.getString("c_mobile");
+                int cCredit = resultSet.getInt("c_credit");
+
+                 customer = new Customer(id, cName, cEmail, passwd, cJob, cAddress, cMobile, cCredit, null, null, null);
+           
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Customer_Dao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return customer;
+    }
 }
--- OnlineBookShop/src/java/com/servlets/Signup.java
+++ OnlineBookShop/src/java/com/servlets/Signup.java
@@ -6,12 +6,18 @@
 package com.servlets;
 
 import com.beans.Customer;
+import com.daos.Customer_Dao;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -19,23 +25,69 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Signup extends HttpServlet {
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest requst, HttpServletResponse response) throws ServletException, IOException {
         
         Customer customer=new Customer();
+        Customer_Dao customerDao=new Customer_Dao();
+        Boolean ok = false;
         
-        res.setContentType("text/html");
         
-        customer.setCName(req.getParameter("cName"));
-        customer.setCPassword(req.getParameter("cPassword"));
-        customer.setCJob(req.getParameter("cJob"));
-        customer.setCEmail(req.getParameter("cEmail"));
-        customer.setCAddress(req.getParameter("cAddress"));
-        customer.setCMobile(req.getParameter("cMobile"));
+        response.setContentType("text/html");
         
-        //------------------------starting a new session if user exists---------------------------------
-      //-----------------------number of logged in users------------------------------
-      //req.getSession(true);
-      //after adding data to db redirect to customer page---------------------
+        //----------filling customer object with data from form--------------
+        customer.setCName(requst.getParameter("cName"));
+        customer.setCPassword(requst.getParameter("cPassword"));
+        customer.setCJob(requst.getParameter("cJob"));
+        customer.setCEmail(requst.getParameter("cEmail"));
+        customer.setCAddress(requst.getParameter("cAddress"));
+        customer.setCMobile(requst.getParameter("cMobile"));
+        
+        
+        try {
+            //----------------insert data to Db------------
+           ok= customerDao.signUp(customer);
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        if(ok){
+            
+            try {
+               
+                
+                
+               customer=customerDao.login(customer.getCEmail(),customer.getCPassword());
+                
+
+               
+                if(customer !=null){
+                   
+                   HttpSession session = requst.getSession(true);
+                   
+                   session.setAttribute("customer",customer);
+                   
+                   response.sendRedirect("customerSite/profile.jsp");
+               }
+               
+               else{
+                    response.sendRedirect("customerSite/checkout.jsp");
+//              RequestDispatcher rd = req.getRequestDispatcher("customerSite/checkout.jsp");
+//              rd.forward(req, res);
+                   
+               }
+                          
+            } catch (SQLException ex) {
+                Logger.getLogger(Signup.class.getName()).log(Level.SEVERE, null, ex);
+            }
+             
+            
+        }
+        else{
+            response.sendRedirect("customerSite/checkout.jsp");
+        }
+        
         
     }
 }
